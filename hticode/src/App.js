924861a540c1c9d0912ffb8f9a7f2a774ea83d21
ZLaronM <zachary.mitchell.16.ctr@us.af.mil>
2021-02-03 10:51:40 -0600

    First Commit

---
+++ hticode/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { CaseOverview } from './components/case-overview';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <CaseOverview />
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ hticode/src/classes/case.js
@@ -0,0 +1,7 @@
+class Case {
+    constructor(agenciesList, caseNum, demographics) {
+        this.agencies = agenciesList;
+        this.caseNum = caseNum;
+        this.demo = demographics;
+    }
+}
